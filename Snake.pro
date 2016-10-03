f9cf09b2e34f941f4471386c137e9584e3944f73
Suri Zhang <surizhang1224@gmail.com>
2016-10-02 22:51:21 -0400

    Add files via upload

---
+++ Snake.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-07-23T09:23:43
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = Snake
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        SnakeDlg.cpp
+
+HEADERS  += SnakeDlg.h
+
+FORMS    += SnakeDlg.ui
+
+RESOURCES += \
+    Snake.qrc
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <QTextCodec>
+#include <QTime>
+#include <QTranslator>
+#include <QLocale>
+#include <QtGui/QApplication>
+#include "SnakeDlg.h"
+
+int main(int argc, char *argv[])
+{
+    QTextCodec::setCodecForTr (
+        QTextCodec::codecForName ("utf-8"));
+    qsrand (QTime::currentTime ().msec ());
+
+    QApplication a(argc, argv);
+
+    QTranslator tr;
+    tr.load ("qt_" + QLocale::system ().name (),
+        "/usr/share/qt4/translations");
+    a.installTranslator (&tr);
+
+    SnakeDlg w;
+    w.show();
+    
+    return a.exec();
+}
